cabf84d23597d3a15d04c8b0f1556a5e2621c5c9
jahan-d <jahanebnadelower@gmail.com>
2025-09-05 17:17:24 +0600

    vocabulary half done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all")
-    .then(res => res.json())
-    .then(json => displayLesson(json.data))
+        .then(res => res.json())
+        .then(json => displayLesson(json.data))
 }
 
 const displayLesson = lessons => {
@@ -10,13 +10,40 @@ const displayLesson = lessons => {
     lessons.forEach(lesson => {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-            <button class="btn btn-outline btn-primary">
+            <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
                 <i class="fa-solid fa-book-open"></i>
                 Lesson ${lesson.level_no}
             </button>
         `;
 
         lessonsContainer.append(btnDiv);
+    })
+}
+
+const loadLevelWord = levelNo => {
+    fetch(`https://openapi.programming-hero.com/api/level/${levelNo}`)
+        .then(res => res.json())
+        .then(json => displayLevelWord(json.data))
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = ``;
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word?word.word:"Not found"}</h2>
+            <p class="font-semibold" >Meaning /Pronounciation</p>
+            <div class="font-medium font-bangla">${word.meaning? word.meaning:"not found"} / ${word.pronunciation?word.pronunciation: "not found"}</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10]mhover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `;
+        wordContainer.append(card);
     })
 }
 
